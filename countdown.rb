7eacbfa0d8e79585f8d260c162468ad4aaa1571e
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-08-22 16:48:18 +0300

    commit tree 6a3310bed3423c643bb9b0e31a6580c634c2c207

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(my_int)
+    while my_int >= 1
+        puts"#{my_int} SECOND(S)!"
+        my_int -= 1
+    end 
+    return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(my_int)
+    while my_int >= 1
+        sleep(my_int)
+        puts"#{my_int} SECOND(S)!"
+        my_int -= 1
+    end 
+    return "HAPPY NEW YEAR!"
 end
